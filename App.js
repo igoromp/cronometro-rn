edc4b97bddf1dbc770419f8bbf78765b8e5f8a68
Igor Oliveira <igoroliveira@Mac-mini-de-Igor.local>
2018-09-17 17:48:57 -0300

    [Cronometro] Implementando App

--- App.js
+++ App.js
@@ -1,49 +1,101 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
-
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import RelogioImg from './images/relogio.png';
+import {StyleSheet, Text, View,Image,TouchableOpacity} from 'react-native';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+export default class App extends Component{
+  constructor(props){
+    super(props);
+    this.timer=null;
+    this.state ={
+      n:0,
+      botao:'Iniciar'
+    };
+    
 
-type Props = {};
-export default class App extends Component<Props> {
+    this.runTimer = this.runTimer.bind(this);
+  }
+
+  runTimer(){
+    let state = this.state;
+
+    this.timer = (this.timer != null) 
+                ?
+                  ()=>{
+                    clearInterval(this.timer);
+                    return this.timer = null
+                  }
+                :
+                  ()=>{
+                    this.timer = setInterval(()=>{
+                      let state = this.state;
+                      state.n +=0.1;
+                      this.setState(s);
+                    },100)
+                  };
+
+
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+          <View style={styles.imgArea}>
+             <Image  source={RelogioImg} style={styles.imgRelogio}/>
+             <Text style={styles.txtRelogio}>{this.state.n.toFixed(1)}</Text>
+
+             <View style={styles.btnArea}>
+                <TouchableOpacity  style={styles.btn} onPress={this.runTimer}>
+                    <Text style={styles.btnLabel}>{this.state.botao}</Text>
+                </TouchableOpacity>
+                <TouchableOpacity  style={styles.btn} onPress={()=>console.log('test')}>
+                    <Text style={styles.btnLabel}>Limpar</Text>
+                </TouchableOpacity>
+             </View>  
+          </View>
       </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    container:{
+      flex:1,
+      backgroundColor:'#370e54',
+     
+    },
+    imgArea:{
+      flex:1,
+      alignItems:'center',
+      justifyContent:'center',
+    },
+    imgRelogio:{
+        alignItems:'center'      
+    },
+    btnArea:{
+      
+      flexDirection:'row',
+      backgroundColor:'transparent',
+      height:40,
+      marginTop:120
+    },
+    btn:{
+      backgroundColor:'#c08f29',
+      flex:1,
+      alignItems:'center',
+      justifyContent:'center',
+      marginLeft:10,
+      marginRight:10,
+    },
+    btnLabel:{
+      textAlign:'center',
+      fontSize:18, 
+      color:'white',
+      fontWeight:'bold'     
+    },
+    txtRelogio:{
+      color:'#d1b738',
+      fontSize:70,
+      fontWeight:'bold',
+      marginTop:-145
+    }
 });
